fa54b679bcf627210b35fe232c23ded4ed65bc6d
Dan Losev <losev_da@nntu.ru>
2023-04-11 01:04:20 +0300

    fix

--- dtos/violationsDto.js
+++ dtos/violationsDto.js
@@ -10,19 +10,17 @@ module.exports = class ViolationsDto {
 
 
   constructor(...model) {
-    console.log('model[0].dataValues.id: ', model[0].dataValues.id)
-    console.log('model[1].dataValues.id: ', model[1].dataValues.id)
-    this.id = model[0].dataValues.id
-    this.name = model[0].dataValues.name
-    this.description = model[0].dataValues.description
-    this.latitude = model[0].dataValues.latitude
-    this.longitude = model[0].dataValues.longitude
-    this.violationStatus = model[0].dataValues.violationStatus
-    this.isAcceptedClass = model[0].dataValues.isAcceptedClass
-    this.file = model[0].dataValues?.file
+    this.id = model[0].id
+    this.name = model[0].name
+    this.description = model[0].description
+    this.latitude = model[0].latitude
+    this.longitude = model[0].longitude
+    this.violationStatus = model[0].violationStatus
+    this.isAcceptedClass = model[0].isAcceptedClass
+    this.file = model[0]?.file
     this.garbageClass = {
-      id: model[1].dataValues.id,
-      name: model[1].dataValues.name
+      id: model[1].id,
+      name: model[1].name
     }
   }
 }
