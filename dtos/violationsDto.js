0053d2bac2dba8cc6c190429fcd490ce9b0c75f0
Dan Losev <losev_da@nntu.ru>
2023-04-11 00:53:11 +0300

    fix

--- dtos/violationsDto.js
+++ dtos/violationsDto.js
@@ -15,24 +15,10 @@ module.exports = class ViolationsDto {
     this.latitude = model[0].latitude
     this.longitude = model[0].longitude
     this.violationStatus = model[0].violationStatus
-    if (model[0]?.file) {
-      this.file = this.#createFileObj(model[0].file)
-    }
+    this.file = model[0]?.file
     this.garbageClass = {
       garbageClassId: model[1].garbageClassId,
       garbageClassName: model[1].garbageClassName
     }
-  }
-
-  #createFileObj(files) {
-    let obj = {}
-    files.forEach((file, iter) => {
-      const originalName = file.split('-')
-      obj[iter] = {
-        "name": originalName[originalName.length - 1],
-        "link": file
-      }
-    })
-    return obj
   }
 }
